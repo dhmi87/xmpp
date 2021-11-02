dd6aecda40c02603a6e6b20e330a5ce57e88fa60
Abdulrahman Alosaimi <ah.osaimi@hotmail.com>
2021-11-03 01:47:36 +0300

    clean files

--- index.js
+++ index.js
@@ -9,44 +9,24 @@ const a = new xmpp('xmpps://xmpp.jp:5223', 'a.osaimi', 'Xmp@1598753');
 
 a.activeDebug(false, true);
 
-function print(contacts) {
-	contacts.forEach((element) => {
-		switch (element.subscription) {
-			case 'both':
-				logger.info(`[frind] ${element.jid}`);
-				break;
-			case 'from':
-				logger.info(`[from] ${element.jid}`);
-				break;
-			case 'to':
-				logger.info(`[to] ${element.jid}`);
-				break;
-			case 'none':
-				logger.warn(`[none] ${element.jid}`);
-				break;
-			default:
-				logger.info(`[unkown] ${element.jid}`);
-				break;
-		}
-	});
-}
-
-function refresh(params) {
+function printContact(contact) {
 	console.log('contacts');
-	console.log(a.contacts);
+	console.log(contact);
+	//a.unBlockContact('ah.osaimi@xmpp.jp');
 }
 
 async function doconn() {
 	try {
-		const joinCallEventHandler = () => {
+		const joinCallEventHandler = (room) => {
 			logger.info('joinCallEventHandler from client');
+			logger.info(room);
 		};
 
 		a.addEventListener(a.eventList.VIDEOCALL, joinCallEventHandler);
-		a.addEventListener(a.eventList.CONTACT_STATUS_CHANGED, refresh);
+		a.addEventListener(a.eventList.CONTACT_STATUS_CHANGED, printContact);
 		await a.connect();
 
-		//logger.info(await a.getVCard('ah.osaimi@xmpp.jp'));
+		await a.removeContact('a@a.a');
 	} catch (error) {
 		console.log(error);
 	}
